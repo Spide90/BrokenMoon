95d4615937a40a67e28ba2eb8331160471b3b4a0
simpae <s.paehler@hbksaar.de>
2013-06-18 13:08:50 +0200

    kl änderungen

--- BrokenMoon/Assets/Scripts/Player/BulletMove.js
+++ BrokenMoon/Assets/Scripts/Player/BulletMove.js
@@ -1,11 +1,12 @@
 #pragma strict
 
 function Start () {
+	rigidbody.AddRelativeForce(Vector3.forward * 50);
 }
 
 function Update () {
 	//simple forward move for bullet
-	transform.Translate(Vector3.forward * 50 * Time.deltaTime);
+	//transform.Translate(Vector3.forward * 50 * Time.deltaTime);
 	//collider.Move(Vector3.forward * 50 * Time.deltaTime);
-	Debug.Log(renderer.isVisible);
+	//Debug.Log(renderer.isVisible);
 }
